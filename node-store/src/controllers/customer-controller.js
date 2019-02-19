984fe10048b9c6feebcf50201909da2e6ddfcce3
VagnerAlcantara <vagnerjosedea.ferreira@outlook.com>
2019-02-19 08:13:54 -0300

    Aplicando autentication and CORS

--- node-store/src/controllers/customer-controller.js
+++ node-store/src/controllers/customer-controller.js
@@ -22,7 +22,8 @@ exports.post = async (req, res, next) => {
         await repository.create({
             name: req.body.name,
             email: req.body.email,
-            password: md5(req.body.password + global.SALT_KEY)
+            password: md5(req.body.password + global.SALT_KEY),
+            roles: ["user"]
         });
 
         emailService.send(
@@ -60,7 +61,8 @@ exports.authenticate = async (req, res, next) => {
         const token = await authService.generateToken({
             id: customer.id,
             email: customer.email,
-            name: customer.name
+            name: customer.name,
+            roles: customer.roles
         });
 
         res.status(201).send({
@@ -92,7 +94,7 @@ exports.refresh = async (req, res, next) => {
 
         const customer = await repository.getById(data.id);
 
-        
+
         if (!customer) {
             res.status(401).send({
                 message: "Cliente não encontrado"
@@ -103,7 +105,8 @@ exports.refresh = async (req, res, next) => {
         const tokenData = await authService.generateToken({
             id: customer.id,
             email: customer.email,
-            name: customer.name
+            name: customer.name,
+            roles: customer.roles
         });
 
         res.status(201).send({
